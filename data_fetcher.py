0eeb21b6fac23f458cee1b037c8ec34b3e7a7e9c
Eric Draegerdt <eric.draegerdt@posteo.de>
2025-09-05 17:45:30 +0200

    minor refactoring

--- data_fetcher.py
+++ data_fetcher.py
@@ -9,10 +9,10 @@ URL = 'https://api.api-ninjas.com/v1/animals'
 
 
 def fetch_data(animal_name):
-  """
-  Fetches the animals data for the animal 'animal_name'.
-  Returns: a list of animals, each animal is a dictionary.
-  """
-  res = requests.get(url=URL, params={'name': animal_name}, headers={'X-API-Key': API_KEY})
-  data = res.json()
-  return data
+    """
+    Fetches the animals data for the animal 'animal_name'.
+    Returns: a list of animals, each animal is a dictionary.
+    """
+    res = requests.get(url=URL, params={'name': animal_name}, headers={'X-API-Key': API_KEY})
+    data = res.json()
+    return data
