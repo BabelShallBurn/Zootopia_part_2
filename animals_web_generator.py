0ab473a13a2a3b68b7aa2adbc2b2e10e4bd7f7a2
Eric Draegerdt <eric.draegerdt@posteo.de>
2025-09-05 18:03:11 +0200

    added html-generation

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,5 +1,78 @@
 import data_fetcher
 
-animal_name = input("Please enter an animal: ")
+def load_html(file_path):
+    """ Loads a HTML file """
+    with open(file_path, "r", encoding="utf-8") as fileobj:
+        html_content = fileobj.read()
+        return html_content
 
-data = data_fetcher.fetch_data(animal_name)
+
+def serialize_animal(animal):
+    """serializes animal and returns neccessary data in list form
+
+    Args:
+        animal (dict): holds information of one animal
+
+    Returns:
+        list: list with easy to access information
+    """
+    animal_obj = []
+    animal_obj.append(animal["name"])
+    animal_obj.append(["Diet: ", animal["characteristics"]["diet"]])
+    animal_obj.append(["Locations: ", animal['locations']])
+    if "type" in animal['characteristics']:
+        animal_obj.append(["Type: ", animal["characteristics"]["type"]])
+    if "distinctive_feature" in animal['characteristics']:
+        animal_obj.append(["Distinctive feature: ", animal["characteristics"]["distinctive_feature"]])
+    if "temperament" in animal['characteristics']:
+        animal_obj.append(["Temperament: ", animal["characteristics"]["temperament"]])
+    if "lifespan" in animal['characteristics']:
+        animal_obj.append(["Lifespan: ", animal["characteristics"]["lifespan"]])
+    return animal_obj
+
+
+def create_html(serialized_animal):
+    """creates html string from serialized animal
+
+    Args:
+        serialized_animal (list): list with animal information
+
+    Returns:
+        string: html string
+    """
+    animal_obj = ''
+    animal_obj += '<li class="cards__item">\n'
+    animal_obj += f'  <div class="card__title">{serialized_animal[0]}</div>\n'
+    animal_obj += '  <div class="card__text">\n'
+    animal_obj += '    <ul>\n'
+    for item in serialized_animal:
+        if len(item) == 2:
+            if isinstance(item[1],list):
+                animal_obj += f'      <li><strong>{item[0]}</strong>{", ".join(item[1])}\n'
+            else:
+                animal_obj += f'      <li><strong>{item[0]}</strong>{item[1]}\n'
+    animal_obj += "    </ul>\n"
+    animal_obj += "  </div>\n"
+    animal_obj += '</li>\n'
+    animal_obj += '\n'
+    return animal_obj
+
+def main():
+    animal_name = input("Please enter an animal: ")
+    animals_data = data_fetcher.fetch_data(animal_name)
+
+    html_data = load_html('animals_template.html')
+
+    animals_data_string = ""
+
+    for animal in animals_data:
+        serialized_animal = serialize_animal(animal)
+        animals_data_string += create_html(serialized_animal)
+
+    html_data = html_data.replace("__REPLACE_ANIMALS_INFO__", animals_data_string)
+
+    with open("animals_data.html", "w", encoding="utf-8") as handle:
+        handle.write(html_data)
+
+if __name__ == "__main__":
+    main()
